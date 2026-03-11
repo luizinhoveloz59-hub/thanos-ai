d4b11d692b4df0bfe1efaff9e187deeb4ae82b75
luizinhoveloz59-hub <luizinhoveloz59@gmail.com>
2026-03-11 12:19:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 600
+  });
+
+  win.loadFile('login.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
